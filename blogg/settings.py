8f1b3fb9af53ec678cdc3f864b4d404d0b994ee8
JOD4004 <109351785+JOD4004@users.noreply.github.com>
2022-07-16 19:36:50 +0545

    Update settings.py

--- blogg/settings.py
+++ blogg/settings.py
@@ -132,9 +132,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 #Email Credentials
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = "gautamroshan4004@gmail.com"
-EMAIL_HOST_PASSWORD = "ubddwmktxawnvnts"
+
